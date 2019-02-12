b5a89f7147cc9d0caead2efc16c07deab205664e
dmaiocchi <dmaiocchi@suse.com>
2019-02-13 00:22:59 +0100

    add some cli experimental

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use structopt::StructOpt;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(StructOpt)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    #[structopt(parse(from_os_str))]
+    file_path: std::path::PathBuf,
 }
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Cli::from_args();
+    let path = "caaspctl.yml";
+    let content = std::fs::read_to_string(path)?;
+    println!("file content: {}", content);
+    Ok(())
+}
+
